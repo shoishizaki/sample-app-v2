402f8808ef563d6c5504b55c8c7b45d6d92ba60f
EC2 Default User <ec2-user@ip-172-31-18-29.us-east-2.compute.internal>
2020-03-19 14:19:30 +0000

    Create all users view page

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,5 +1,43 @@
 <template>
   <div>
-    Users
+    <div>
+    <h1>All Users</h1>
+    <hr>
+    </div>
+    <div v-for="user in users" :key="user.name">
+      <div>UserName: {{user.fields.username.stringValue}}</div>
+      <div>Email: {{user.fields.email.stringValue}}</div>
+      <br>
+      <hr>
+    </div>
   </div>
+  
 </template>
+
+<script>
+import axios from 'axios';
+  
+export default {
+  computed: {
+      idToken() {
+        return this.$store.state.idToken;
+      }
+  },
+  data() {
+      return{
+        users: [],
+      };
+  },
+  created() {
+    axios.get('https://firestore.googleapis.com/v1/projects/sample-vuejs-70946/databases/(default)/documents/users',{
+      headers: {
+        Authorization: `Bearer ${this.idToken}`
+      }
+    })
+    .then(response => {
+      this.users = response.data.documents;
+      console.log(this.users);
+    });
+  }  
+};
+</script>
